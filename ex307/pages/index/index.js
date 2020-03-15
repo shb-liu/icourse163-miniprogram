08d20ac2392104a5cd38fa7b580e03108aa2a3fe
mzm-l <tefodliu@gmail.com>
2020-03-15 18:32:37 +0800

    ex307-ex309

--- ex307/pages/index/index.js
+++ ex307/pages/index/index.js
@@ -1,1 +1,11 @@
 //index.js
+Page({
+  data: {
+    array: ["张三", "李四", "王五", "赵六"],
+    object: {
+      name: "Mr.T",
+      gender: "Male",
+      country: "China"
+    }
+  }
+})
---
+++ ex309/pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+Page({
+  data: {
+    stu01: {
+      name: "aaa",
+      age: "12",
+      gender: "m"
+    },
+    stu02: {
+      name: "bbb",
+      age: "13",
+      gender: "w"
+    }
+  }
+})
